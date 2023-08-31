622368a0f776a6c11b814dc3c73b332bc5456bcb
maggiechangjx <maggiechangjx@gmail.com>
2023-08-31 16:08:04 -0400

    fit images for mobile

---
+++ gatsby-browser.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+exports.shouldUpdateScroll = ({
+    routerProps: { location },
+    getSavedScrollPosition
+  }) => {
+    const currentPosition = getSavedScrollPosition(location)
+    const queriedPosition = getSavedScrollPosition({ pathname: `/random` })
+  
+    window.scrollTo(...(currentPosition || [0, 0]))
+  
+    return true
+  }
+
